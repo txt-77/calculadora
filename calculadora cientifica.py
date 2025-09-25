df7e7a59005235104ffb0b3bbd7a231866691be5
txt-77 <gustavomartins717117@gmail.com>
2025-09-24 23:35:09 -0300

    correçao
    funçoes usando lambda quando nao necessario

--- calculadora cientifica.py
+++ calculadora cientifica.py
@@ -134,7 +134,7 @@ Button(frame6, text="nCr", width=3, height=1, font=fontBotao).grid(row=3, column
 
 Label(frame6, text="Rec(", fg="orange", font=fontMini).grid(row=2, column=4, sticky="sw",pady=1)
 Label(frame6, text=":", fg="red", font=fontMini).grid(row=3, column=4, sticky="se",pady=1)
-Button(frame6, text="Pol(", width=3, height=1, font=fontBotao,command=lambda: botao_pol).grid(row=3, column=4, sticky="s" ,pady=1)
+Button(frame6, text="Pol(", width=3, height=1, font=fontBotao,command=botao_pol).grid(row=3, column=4, sticky="s" ,pady=1)
 
 Label(frame6, text="³√x", fg="orange", font=fontMini).grid(row=2, column=5, sticky="s" ,pady=1)
 Button(frame6, text="x³", width=3, height=1, font=fontBotao).grid(row=3, column=5, sticky="s" ,pady=1)
@@ -176,7 +176,7 @@ Button(frame6, text="sin", width=3, height=1, font=fontBotao).grid(row=7, column
 
 Label(frame6, text="E", fg="red", font=fontMini).grid(row=6, column=4, sticky="se" ,pady=1)
 Label(frame6, text="cos⁻¹", fg="orange", font=fontMini).grid(row=6, column=4, sticky="sw" ,pady=1)
-Button(frame6, text="cos", width=3, height=1, font=fontBotao,command=lambda: botao_cos).grid(row=7, column=4, pady=1)
+Button(frame6, text="cos", width=3, height=1, font=fontBotao,command=botao_cos).grid(row=7, column=4, pady=1)
 
 Label(frame6, text="F", fg="red", font=fontMini).grid(row=6, column=5, sticky="se" ,pady=1)
 Label(frame6, text="tan⁻¹", fg="orange", font=fontMini).grid(row=6, column=5, sticky="sw" ,pady=1)
@@ -243,9 +243,10 @@ Label(frame5, text="π", fg="orange", font=fontMini).grid(row=16, column=2, stic
 Button(frame5, text="EXP", width=5, height=2, font=fontBotao).grid(row=17, column=2, padx=2, pady=2, sticky="s")
 
 Label(frame5, text="DRG►", fg="orange", font=fontMini).grid(row=16, column=3, sticky="s", padx=2, pady=2)
-Button(frame5, text="Ans", width=5, height=2, font=fontBotao,command=lambda:botao_ans).grid(row=17, column=3, sticky="s", padx=2, pady=2)
+Button(frame5, text="Ans", width=5, height=2, font=fontBotao,command=botao_ans).grid(row=17, column=3, sticky="s", padx=2, pady=2)
 
 Label(frame5, text="%", fg="orange", font=fontMini).grid(row=16, column=4, sticky="s", padx=2, pady=2)
 Button(frame5, text="=", width=5, height=2, font=fontBotao, command=botao_igual).grid(row=17, column=4, sticky="s", padx=2, pady=2)
+
 
 root.mainloop()
